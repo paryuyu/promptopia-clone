a8a6894be2f6538b13f87a3ed7c9770877dc6e04
paryuyu <107971288+paryuyu@users.noreply.github.com>
2023-06-27 16:28:56 +0900

    알림창추가

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -17,7 +17,7 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
 
     if (post.creator._id === session?.user.id) return router.push("/profile");
     
-    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+   alert("본인의 프로필만 확인 가능합니다.")
   };
 
   const handleCopy = () => {
